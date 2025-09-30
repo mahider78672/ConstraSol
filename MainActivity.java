075c27ced5ee8ca6e84a651b5a9211b77aff8327
Mohd Haider <120472415+mahider78672@users.noreply.github.com>
2025-09-30 19:53:17 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 }
+
 
